18f83631cca0d760c2aa3a8fb146c3a8d868304d
Nate Lao <lao.nathan@yahoo.com>
2026-07-13 23:10:05 -0500

    testing - person detection

--- main.cpp
+++ main.cpp
@@ -1,3 +1,6 @@
+#include <thread>
+#include <chrono>
+#include <algorithm>
 
 /* Image Detection */
 #include <wiringPi.h>
@@ -7,9 +10,11 @@
 #include <opencv2/dnn.hpp>
 
 /* Datasets and Settings */
+const auto FRAME_DELAY = std::chrono::milliseconds(50);
+const auto ACTIVE_LATCH = std::chrono::seconds(3);
 const std::string DATASET_PROTOTXT = "dataset/MobileNetSSD_deploy.prototxt";
 const std::string DATASET_CAFFEMODEL = "dataset/MobileNetSSD_deploy.caffemodel";
-enum class DATASET_OBJECT_LABEL
+enum class DATASET_OBJECT_LABEL : size_t
 {
     background,
     aeroplane,
@@ -48,17 +53,35 @@ public:
         net.setPreferableTarget(cv::dnn::DNN_TARGET_CPU);
     }
 
-    bool detect(const DATASET_OBJECT_LABEL label, const int confidence=0.5)
+    bool detect(const std::vector<DATASET_OBJECT_LABEL> &labels, const int confidence = 0.5)
     {
+        /* Camera Frame Capture*/
         cap >> frame;
         assert(!frame.empty());
 
+        /* Run Detection Algorithm */
         cv::Mat blob = cv::dnn::blobFromImage(frame, 0.007843, cv::Size(300, 300), cv::Scalar(127.5, 127.5, 127.5), false);
         net.setInput(blob);
         cv::Mat detection = net.forward();
 
+        /* Parse Detection Results */
+        bool result = false;
+        cv::Mat detectionMat(detection.size[2], detection.size[3], CV_32F, detection.ptr<float>());
+        for (int i = 0; i < detectionMat.rows; ++i)
+        {
+            const float DETECTED_OBJECT_CONFIDENCE = detectionMat.at<float>(i, 2);
+            if (DETECTED_OBJECT_CONFIDENCE > confidence)
+            {
+                const size_t DETECTED_OBJECT_INDEX = static_cast<size_t>(detectionMat.at<float>(i, 1));
+                result = result ||
+                         (std::count(
+                              labels.begin(),
+                              labels.end(),
+                              static_cast<DATASET_OBJECT_LABEL>(DETECTED_OBJECT_INDEX)) > 0);
+            }
+        }
 
-        return false;
+        return result;
     }
 
 private:
@@ -69,11 +92,18 @@ private:
 
 int main()
 {
+    /** @todo this could be optimized to a hashmap for detect(). */
+    const std::vector<DATASET_OBJECT_LABEL> TARGET_OBJECT_LABELS = {DATASET_OBJECT_LABEL::person};
     CameraDetector camera_a(DATASET_PROTOTXT, DATASET_CAFFEMODEL, 0);
+    CameraDetector camera_b(DATASET_PROTOTXT, DATASET_CAFFEMODEL, 4);
 
+    size_t detection_counter = 0;
     while (true)
     {
-        camera_a.detect(DATASET_OBJECT_LABEL::dog, 0);
+        if (camera_a.detect(TARGET_OBJECT_LABELS) || camera_b.detect(TARGET_OBJECT_LABELS))
+            std::cout << "detected " << detection_counter++ << std::endl;
+
+        std::this_thread::sleep_for(FRAME_DELAY);
     }
 
     return 0;
